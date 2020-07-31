dcf1c712c08b4115069e2a7a401b09ec9b8dbf03
ksk <ksk@ksk-PC>
2020-08-01 02:05:20 +0900

    d

---
+++ vuex/contactsapp_search/src/components/board/Board.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<div>
+			자유게시판
+		</div>
+	</div>
+</template>
--- vuex/contactsapp_search/src/components/user/UserList.vue
+++ vuex/contactsapp_search/src/components/user/UserList.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
-    <table class="table table-default table-hover table-striped" id="list">
-      <thead>
-        <tr>
-          <th class="text-center">아이디</th>
-          <th class="text-center">이름</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users">
-          <td class="text-center">{{user.id}}</td>
-          <td>{{user.name}}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <button class="btn" @click="getUserList()">조회</button>
+      <p />
+      <table class="table table-default table-hover table-striped" id="list">
+        <thead>
+          <tr>
+            <th class="text-center">아이디</th>
+            <th class="text-center">이름</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" @click="setUser(user)">
+            <td class="text-center">{{user.id}}</td>
+            <td>{{user.name}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div>
+      <div>
+        <label>아이디</label>
+        <div><input type="text" v-model="id" />
+        </div>
+        <label>이름</label>
+        <div><input type="text" v-model="name" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,9 +36,25 @@ import { mapState } from 'vuex';
 
 export default {
   name: 'user-list',
-  //computed : mapState([ 'contacts' ])
+  data: function() {
+    return {
+      id: ""
+      , name: ""
+    }
+  },
   computed: {
-    ...mapState({users: state => state.auth.users})
+    users: function() {
+      return this.$store.state.auth.users;
+    }
+  },
+  methods: {
+    getUserList: function() {
+      this.$store.dispatch("auth/getUserList", {name: null});
+    },
+    setUser: function(user) {
+      this.id = user.id
+      this.name = user.name
+    }
   }
 }
 </script>
